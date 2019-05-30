1eca4ba6f66db414c10f57353a367d830142c56f
Mubashir Ali <mubashir22ali@gmail.com>
2019-05-30 15:17:18 +0500

    fixed

--- src/containers/index.js
+++ src/containers/index.js
@@ -6,7 +6,7 @@ import Login from './Login';
 import LoginHoc from './LoginHoc';
 import AuthLoading from './AuthLoading'
 import CallPlans from './CallPlans'
-import Home from './Home
+import Home from './Home'
 
 export {
     Login,
