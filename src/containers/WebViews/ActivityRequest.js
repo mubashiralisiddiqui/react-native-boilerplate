7997d7a19fb2fada844a727b3440cdb2fd29e168
Muhammad Nauman <naumancs@outlook.com>
2019-09-12 11:13:11 +0500

    wip

---
+++ src/containers/WebViews/ActivityRequest.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { WebView } from 'react-native-webview';
+import { NetworkContext } from '../../components/NetworkProvider';
+import { getUser } from '../../reducers/authReducer';
+import {connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import { navigationOption, jsCodeForWebViews } from '../../constants';
+import { ScreenLoader } from '../../components';
+
+class ActivityRequest extends Component {
+    state = {
+        loading: true
+    }
+    static contextType = NetworkContext;
+    static navigationOptions = ({ navigation }) => (navigationOption(navigation, 'Activity Request'))
+
+
+    getUrl = () => {
+        return 'http://portal.hudsonpharma.com/Login.aspx?ReturnURL=/Pages/HusonSSR/ActivityRequest.aspx'
+    }
+    componentDidMount() {
+        this.context.hideRefresh();
+    }
+    render() {
+        let string = jsCodeForWebViews(this.props.user);
+        return (
+
+            <WebView
+                source={{ uri: this.getUrl() }}
+                injectedJavaScript={string}
+                javaScriptEnabled={true}
+                renderLoading={ScreenLoader}
+                startInLoadingState={true}
+            />
+        )
+    }
+}
+/**
+ * @const function mapStateToProps
+ * @description It will map the redux state to this component
+ * @param {*} state
+ * @returns Object
+ * @memberof CallPlans
+ * @author Muhammad Nauman <muhammad.nauman@hudsonpharma.com>
+ */
+const mapStateToProps = state => {
+    return {
+        user: getUser(state),
+    }
+}
+
+/**
+ * @const function mapDispatchToProps
+ * @description It will actionable redux methods to this component
+ * @param {*} dispatch
+ * @returns Object
+ * @memberof CallPlans
+ * @author Muhammad Nauman <muhammad.nauman@hudsonpharma.com>
+ */
+const mapDispatchToProps = dispatch => bindActionCreators({
+    // 
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActivityRequest)
